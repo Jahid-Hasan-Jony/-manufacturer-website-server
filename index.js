3915bca958b63cfdf67099459337d211ac667a96
Jahid-Hasan-Jony <jahidhasanjony48@gmail.com>
2022-05-21 21:20:23 +0600

    initialize project and add api

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const jwt = require('jsonwebtoken');
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+app.use(express.json());
+app.use(cors());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.deudh.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: "unAuthorized Access" });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+};
+
+async function run() {
+    try {
+        await client.connect()
+        const assignment12Collection = client.db('assignment12').collection('data');
+        const userCollection = client.db('assignment12').collection('users');
+
+        // get all data
+        app.get('/data', async (req, res) => {
+            const query = {};
+            const cursor = assignment12Collection.find(query)
+            const allData = await cursor.toArray();
+            res.send(allData);
+        });
+
+        //update or insert data
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updatedoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updatedoc, options);
+            res.send(result);
+        });
+
+    }
+    finally { }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
